44b493666a59c6d9c79d4b518c93544ffa25f754
Gordon Heydon <gordon@heydon.com.au>
2019-07-11 17:03:35 +1000

    Create build process to handle multiple versions of PHP
    Also added PHP 7.2 version

---
+++ update.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+baseDockerfile=Dockerfile.template
+versions=('7.1' '7.2')
+
+generated_warning() {
+  cat <<-EOH
+#
+# NOTE: THIS DOCKERFILE IS GENERATED VIA "update.sh"
+#
+# PLEASE DO NOT EDIT IT DIRECTLY.
+#
+EOH
+}
+
+for version in "${versions[@]}"; do
+  [ -d "$version" ] || continue
+
+  echo "Generating $version/Dockerfile from $baseDockerfile"
+  
+  { generated_warning; cat "$baseDockerfile"; } > "$version/Dockerfile"
+  
+  gsed -ri \
+    -e 's!%%PHP_VERSION%%!'"$version"'!' \
+    "$version/Dockerfile"
+done
